004aaebf2c3ccc93784dc3de411ce6eb1b57abc6
Hereward <hereward@planetonline.com.au>
2016-03-18 15:42:46 +0700

    Tweak to truncate function..

--- ee/expressionengine/third_party/eehive_hacksaw/pi.eehive_hacksaw.php
+++ ee/expressionengine/third_party/eehive_hacksaw/pi.eehive_hacksaw.php
@@ -48,6 +48,8 @@ var $return_data = "";
 	$cutoff = $this->EE->TMPL->fetch_param('cutoff');
 	$append = $this->EE->TMPL->fetch_param('append', '');
 	$allow = $this->EE->TMPL->fetch_param('allow');
+      $html_cut = $this->EE->TMPL->fetch_param('html_cut');
+      $html_max_length = $this->EE->TMPL->fetch_param('html_max_length');
 	
 	if(isset($cutoff) && $cutoff != "") {
 		$cutoff_content = $this->_truncate_cutoff($tag_content, $cutoff, $words, $allow, $append);
@@ -60,7 +62,21 @@ var $return_data = "";
 	} elseif (isset($words) && $words != "") {
 		// Strip the HTML
 		$stripped_content = strip_tags($tag_content, $allow);
-		$new_content = (str_word_count($stripped_content) <= $words ? $stripped_content : $this->_truncate_words($stripped_content, $words, $append));
+		$raw_content = (str_word_count($stripped_content) <= $words ? $stripped_content : $this->_truncate_words($stripped_content, $words, $append));
+/*
+        $dom= new DOMDocument();
+        $dom->loadHTML($raw_content);
+        $xpath = new DOMXPath($dom);
+        $body = $xpath->query('/html/body');
+        $parsed_content = $dom->saveXml($body->item(0));
+*/
+
+        $new_content = $raw_content;
+
+    } elseif (isset($html_cut) && $html_cut != "") {
+        //$new_content = $this->html_cut($tag_content, $html_max_length);
+        $new_content = $this->html_truncate($tag_content, $html_max_length);
+
 	} else {
         // Strip the HTML
 		$stripped_content = strip_tags($tag_content, $allow);
@@ -100,6 +116,192 @@ var $return_data = "";
 	}
     return $content;
   }
+
+
+    function html_truncate($text, $length = 100, $ending = '...', $exact = false, $considerHtml = true) {
+        if (is_array($ending)) {
+            extract($ending);
+        }
+        if ($considerHtml) {
+            if (mb_strlen(preg_replace('/<.*?>/', '', $text)) <= $length) {
+                return $text;
+            }
+            $totalLength = mb_strlen($ending);
+            $openTags = array();
+            $truncate = '';
+            preg_match_all('/(<\/?([\w+]+)[^>]*>)?([^<>]*)/', $text, $tags, PREG_SET_ORDER);
+            foreach ($tags as $tag) {
+                if (!preg_match('/img|br|input|hr|area|base|basefont|col|frame|isindex|link|meta|param/s', $tag[2])) {
+                    if (preg_match('/<[\w]+[^>]*>/s', $tag[0])) {
+                        array_unshift($openTags, $tag[2]);
+                    } else if (preg_match('/<\/([\w]+)[^>]*>/s', $tag[0], $closeTag)) {
+                        $pos = array_search($closeTag[1], $openTags);
+                        if ($pos !== false) {
+                            array_splice($openTags, $pos, 1);
+                        }
+                    }
+                }
+                $truncate .= $tag[1];
+
+                $contentLength = mb_strlen(preg_replace('/&[0-9a-z]{2,8};|&#[0-9]{1,7};|&#x[0-9a-f]{1,6};/i', ' ', $tag[3]));
+                if ($contentLength + $totalLength > $length) {
+                    $left = $length - $totalLength;
+                    $entitiesLength = 0;
+                    if (preg_match_all('/&[0-9a-z]{2,8};|&#[0-9]{1,7};|&#x[0-9a-f]{1,6};/i', $tag[3], $entities, PREG_OFFSET_CAPTURE)) {
+                        foreach ($entities[0] as $entity) {
+                            if ($entity[1] + 1 - $entitiesLength <= $left) {
+                                $left--;
+                                $entitiesLength += mb_strlen($entity[0]);
+                            } else {
+                                break;
+                            }
+                        }
+                    }
+
+                    $truncate .= mb_substr($tag[3], 0 , $left + $entitiesLength);
+                    break;
+                } else {
+                    $truncate .= $tag[3];
+                    $totalLength += $contentLength;
+                }
+                if ($totalLength >= $length) {
+                    break;
+                }
+            }
+
+        } else {
+            if (mb_strlen($text) <= $length) {
+                return $text;
+            } else {
+                $truncate = mb_substr($text, 0, $length - strlen($ending));
+            }
+        }
+        if (!$exact) {
+            $spacepos = mb_strrpos($truncate, ' ');
+            if (isset($spacepos)) {
+                if ($considerHtml) {
+                    $bits = mb_substr($truncate, $spacepos);
+                    preg_match_all('/<\/([a-z]+)>/', $bits, $droppedTags, PREG_SET_ORDER);
+                    if (!empty($droppedTags)) {
+                        foreach ($droppedTags as $closingTag) {
+                            if (!in_array($closingTag[1], $openTags)) {
+                                array_unshift($openTags, $closingTag[1]);
+                            }
+                        }
+                    }
+                }
+                $truncate = mb_substr($truncate, 0, $spacepos);
+            }
+        }
+
+        $truncate .= $ending;
+
+        if ($considerHtml) {
+            foreach ($openTags as $tag) {
+                $truncate .= '</'.$tag.'>';
+            }
+        }
+
+        return $truncate;
+    }
+
+
+    function html_cut($text, $max_length)
+    {
+        $tags   = array();
+        $result = "";
+
+        $is_open   = false;
+        $grab_open = false;
+        $is_close  = false;
+        $in_double_quotes = false;
+        $in_single_quotes = false;
+        $tag = "";
+
+        $i = 0;
+        $stripped = 0;
+
+        $stripped_text = strip_tags($text);
+
+        while ($i < strlen($text) && $stripped < strlen($stripped_text) && $stripped < $max_length)
+        {
+            $symbol  = $text{$i};
+            $result .= $symbol;
+
+            switch ($symbol)
+            {
+                case '<':
+                    $is_open   = true;
+                    $grab_open = true;
+                    break;
+
+                case '"':
+                    if ($in_double_quotes)
+                        $in_double_quotes = false;
+                    else
+                        $in_double_quotes = true;
+
+                    break;
+
+                case "'":
+                    if ($in_single_quotes)
+                        $in_single_quotes = false;
+                    else
+                        $in_single_quotes = true;
+
+                    break;
+
+                case '/':
+                    if ($is_open && !$in_double_quotes && !$in_single_quotes)
+                    {
+                        $is_close  = true;
+                        $is_open   = false;
+                        $grab_open = false;
+                    }
+
+                    break;
+
+                case ' ':
+                    if ($is_open)
+                        $grab_open = false;
+                    else
+                        $stripped++;
+
+                    break;
+
+                case '>':
+                    if ($is_open)
+                    {
+                        $is_open   = false;
+                        $grab_open = false;
+                        array_push($tags, $tag);
+                        $tag = "";
+                    }
+                    else if ($is_close)
+                    {
+                        $is_close = false;
+                        array_pop($tags);
+                        $tag = "";
+                    }
+
+                    break;
+
+                default:
+                    if ($grab_open || $is_close)
+                        $tag .= $symbol;
+
+                    if (!$is_open && !$is_close)
+                        $stripped++;
+            }
+
+            $i++;
+        }
+
+        while ($tags)
+            $result .= "</".array_pop($tags).">";
+
+        return $result;
+    }
   
 
 	// --------------------------------------------------------------------
